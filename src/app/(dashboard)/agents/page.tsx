d7fdd971b2fc0849e63bcf1361f5b2f12a44c2c2
unknown <rishabh.th25@gmail.com>
2025-10-02 20:56:41 +0530

    added ui for page filters, pagination ans synced server and client filters

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -6,11 +6,19 @@ import { HydrationBoundary, dehydrate } from "@tanstack/react-query"
 import { Suspense } from "react"
 import { ErrorBoundary } from "react-error-boundary"
 import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header"
+import { SearchParams } from "nuqs"
+import { loadSearchParams } from "@/modules/agents/params"
 
-const Page = async() => {
+interface Props{
+  searchParams:Promise<SearchParams>,
+}
+
+const Page = async({searchParams}:Props) => {
+
+  const filters=await loadSearchParams(searchParams)
 
   const queryClient=getQueryClient()
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({}))
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({...filters}))
 
   return (
     <>
