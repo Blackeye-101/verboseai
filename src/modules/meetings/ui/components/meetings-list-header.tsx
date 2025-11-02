6b1aacccf2a4cbaa5168d285b63ca033e79aa389
unknown <rishabh.th25@gmail.com>
2025-11-02 13:13:36 +0530

    adding ui for meetings filter

--- src/modules/meetings/ui/components/meetings-list-header.tsx
+++ src/modules/meetings/ui/components/meetings-list-header.tsx
@@ -1,28 +1,61 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { PlusIcon} from "lucide-react"
-import { NewMeetingDialog } from "./new-meeting-dialog"
-import { useState } from "react"
+import { Button } from "@/components/ui/button";
+import { PlusIcon, XCircleIcon } from "lucide-react";
+import { NewMeetingDialog } from "./new-meeting-dialog";
+import { useState } from "react";
+import { MeetingsSearchFilter } from "./meetings-search-filter";
+import { StatusFilter } from "./status-filters";
+import { AgentIdFilter } from "./agent-id-filter";
+import { useMeetingsFilters } from "../../hooks/use-meetings-filters";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { DEFAULT_PAGE } from "@/constants";
 
-export const MeetingsListHeader=()=>{
-    const [isDialogOpen,setIsDialogOpen]=useState(false)
+export const MeetingsListHeader = () => {
+  const [filters, setFilters] = useMeetingsFilters();
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-    return (
-        <>
-            <NewMeetingDialog open={isDialogOpen} onOpenChange={setIsDialogOpen}></NewMeetingDialog> 
-            <div className="py-4 px-4 md:px-8 flex flex-col gap-y-4">
-                <div className="flex items-center justify-between">
-                    <h5 className="font-medium text-xl">My Meetings</h5>
-                    <Button onClick={()=>setIsDialogOpen(true)}>
-                        <PlusIcon></PlusIcon>
-                        New Meeting
-                    </Button>
-                </div>
-                <div className="flex items-center gap-x-2 p-1">
-                   {/* TODO: filters */}
-                </div>
-            </div>
-        </>
-    )
-}
+  const isAnyFilterModified =
+    !!filters.status || !!filters.agentId || !!filters.search;
+
+  const onClearFilters = () => {
+    setFilters({
+      status: null,
+      agentId: "",
+      search: "",
+      page: DEFAULT_PAGE,
+    });
+  };
+
+  return (
+    <>
+      <NewMeetingDialog
+        open={isDialogOpen}
+        onOpenChange={setIsDialogOpen}
+      ></NewMeetingDialog>
+      <div className="py-4 px-4 md:px-8 flex flex-col gap-y-4">
+        <div className="flex items-center justify-between">
+          <h5 className="font-medium text-xl">My Meetings</h5>
+          <Button onClick={() => setIsDialogOpen(true)}>
+            <PlusIcon></PlusIcon>
+            New Meeting
+          </Button>
+        </div>
+        <ScrollArea>
+          <div className="flex items-center gap-x-2 p-1">
+            <MeetingsSearchFilter></MeetingsSearchFilter>
+            <StatusFilter></StatusFilter>
+            <AgentIdFilter></AgentIdFilter>
+            {isAnyFilterModified && (
+              <Button variant="outline" onClick={onClearFilters}>
+                <XCircleIcon className="size-4"></XCircleIcon>
+                Clear
+              </Button>
+            )}
+          </div>
+          <ScrollBar orientation="horizontal"></ScrollBar>
+        </ScrollArea>
+      </div>
+    </>
+  );
+};
